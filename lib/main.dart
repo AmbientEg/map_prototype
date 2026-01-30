4783106292f9073133757b36a66b489bbf2ff4a8
Rodyna Amr <130939400+rodynaamrfathy@users.noreply.github.com>
2026-01-30 19:54:24 +0200

    rendered geojson 2d

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+import 'package:flutter/services.dart';
+
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,6 +23,11 @@ class IndoorNavigationApp extends StatelessWidget {
     );
   }
 }
+
+Future<String> loadGeoJsonFromAssets(String path) async {
+  return await rootBundle.loadString(path);
+}
+
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -51,7 +58,43 @@ class _MapScreenState extends State<MapScreen> {
     );
   }
 
-  void _onMapCreated(MapboxMap mapboxMap) {
+  void _onMapCreated(MapboxMap mapboxMap) async {
     this.mapboxMap = mapboxMap;
+
+    // Move camera
+    await mapboxMap.setCamera(
+      CameraOptions(
+        center: Point(
+          coordinates: Position(31.2014870, 30.0277617),
+        ),
+        zoom: 18.5,
+        pitch: 45,
+      ),
+    );
+
+    // Load GeoJSON from assets
+    final geoJson = await loadGeoJsonFromAssets(
+      'assets/indoor/floor3.geojson',
+    );
+
+    // Add source
+    await mapboxMap.style.addSource(
+      GeoJsonSource(
+        id: 'floor3-source',
+        data: geoJson,
+      ),
+    );
+
+    // Render polygons (rooms, floor shapes, etc.)
+    await mapboxMap.style.addLayer(
+      FillLayer(
+        id: 'floor3-fill',
+        sourceId: 'floor3-source',
+        fillColor: Colors.blue.withOpacity(0.4).value,
+        fillOutlineColor: Colors.blue.value,
+      ),
+    );
   }
+
+
 }
